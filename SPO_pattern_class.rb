f812201ac396952a746e28bd473a837485a87324
Acb897 <camara.alber@gmail.com>
2021-02-17 18:41:31 +0100

    Added methods

--- SPO_pattern_class.rb
+++ SPO_pattern_class.rb
@@ -11,22 +11,52 @@ class SPO
         @SPO_Object = params.fetch(:SPO_Object, "")
     end
     
-    def SPO.query_endpoint(endpoint_URL)
-        @patterns = Hash.new
-        endpoint = endpoint_URL
-        query = <<END
+        sparql = SPARQL::Client.new(endpoint_URL)
+        if mode == "exploratory"
+            #This first query asks for the types of objects inside the triplestore
+            query = <<END
 
-        SELECT DISTINCT(?type)
-        WHERE {
-               ?subject a ?type .
-        } limit 10
+            SELECT DISTINCT(?type)
+            WHERE {
+                   ?subject a ?type .
+            } limit 10
 END
+            result = sparql.query(query)
         
+        elsif mode == "fixed_subject"
+            query = <<END
         
-        sparql = SPARQL::Client.new(endpoint)  # create a SPARQL client
-        result = sparql.query(query)  # Execute query
+            SELECT ?predicate ?object_type
+            WHERE {
+                <#{type}> ?predicate ?object . 
+                ?object a ?object_type.
+            } limit 10
+END
+            result = sparql.query(query)
+
+        elsif mode == "fixed_object"
+            query = <<END
+        
+            SELECT ?predicate ?subject_type
+            WHERE {
+                ?subject ?predicate <#{type}> . 
+                ?subject a ?subject_type.
+            } limit 10
+END
+            result = sparql.query(query)
+        end
+        return result        
+    end
+
+    def SPO.extract_patterns(endpoint_URL)
+        @patterns = Hash.new
+        
         types_array = Array.new
-        result.each do |solution|
+
+        exploration_results = SPO.query_endpoint(endpoint_URL, "exploratory")
+        
+        #The types are stored in an array so that they can be later explored individually in order
+        exploration_results.each do |solution|
             if types_array.include? solution[:type]
                 next
             else
@@ -38,18 +68,9 @@ END
         
         types_array.each do |type|
         
-            #puts "Type: #{type}"
-            query = <<END
-        
-            SELECT ?predicate ?object_type
-            WHERE {
-                <#{type}> ?predicate ?object . 
-                ?object a ?object_type.
-            } limit 10
-END
-        
-            result = sparql.query(query)
-            result.each do |solution|
+            #This query asks for the types of objects and the predicates that interact with each of the types
+            fsubject_results = SPO.query_endpoint(endpoint_URL, "fixed_subject")
+            fsubject_results.each do |solution|
                 if @patterns.include? type
                     @patterns[type] << SPO.new(
                         :SPO_Subject => type,
@@ -64,6 +85,16 @@ END
                     )]
                 end
             end
-        end           
+            fobject_results = SPO.query_endpoint(endpoint_URL, "fixed_object")
+            fobject_results.each do |solution|
+                #falta comprobacion de que no tenga ya ese pattern
+                @patterns[type] << SPO.new(
+                    :SPO_Subject => solution[:subject_type],
+                    :SPO_Predicate => solution[:predicate],
+                    :SPO_Object => type
+                )
+            end
+        end
+        return @patterns          
     end
 end
