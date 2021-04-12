87eec8e595567c829cc4bd816489104d550786b4
Acb897 <camara.alber@gmail.com>
2021-04-12 21:22:24 +0200

    Fixed a typo in the generated data

--- QSF/Query_Matching.rb
+++ QSF/Query_Matching.rb
@@ -4,6 +4,6 @@ _:ABC some:predicate _:123
 _:123 a <object_type>
 =end
 def fake_data_generator(subject_type, predicate, object_type)
-    data = "_:ABC a <#{subject_type}>\n_:ABC #{predicate} _:123\n_:123 a <#{object_type}>"
+    data = "_:ABC a <#{subject_type}>. \n_:ABC #{predicate} _:123. \n_:123 a <#{object_type}>"
     return data
 end
