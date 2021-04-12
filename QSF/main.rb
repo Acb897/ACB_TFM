87f4279a936e30c8cd5c6f9ba4843b97e7e7b09a
Acb897 <camara.alber@gmail.com>
2021-04-12 21:06:27 +0200

    Some changes and added fake data generation function

---
+++ QSF/main.rb
@@ -0,0 +1,4 @@
+require "./Query_Matching.rb"
+
+test = fake_data_generator("sw:protein", "sw:taxon", "sw:NCBI_taxon")
+print test
--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ require "./SPO_pattern_class.rb"
 
 engine = Engine.new()
 
-first_test = engine.extract_patterns("https://rdf.metanetx.org/sparql")
+endpoint = "https://rdf.metanetx.org/sparql"
+#endpoint = "http://sparql.uniprot.org/sparql"
+first_test = engine.extract_patterns(endpoint)
 #first_test = engine.extract_patterns("http://sparql.uniprot.org/sparql")
-shacl_test = engine.shacl_generator(first_test, "output_test.ttl")
+shacl_test = engine.shacl_generator(first_test, "test.ttl")
 
