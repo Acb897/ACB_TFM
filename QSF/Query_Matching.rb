29faa13d19cde3487a80d253d2aae055b9081111
Acb897 <camara.alber@gmail.com>
2021-04-14 20:56:39 +0200

    Implemented the validation of RDF against SHACL

--- QSF/Query_Matching.rb
+++ QSF/Query_Matching.rb
@@ -3,7 +3,21 @@ _:ABC a <subject_type>
 _:ABC some:predicate _:123
 _:123 a <object_type>
 =end
-def fake_data_generator(subject_type, predicate, object_type)
+
+require 'linkeddata'
+require 'shacl'
+
+def fake_data_generator(subject_type, predicate, object_type, output_document)
     data = "_:ABC a <#{subject_type}>. \n_:ABC #{predicate} _:123. \n_:123 a <#{object_type}>"
+    File.open(output_document, "w") {|file|
+        file.write data}
     return data
+end
+
+def shacl_validator(rdf_graph, shacl_document)
+    graph = RDF::Graph.load(rdf_graph)
+    shacl = SHACL.open(shacl_document)
+    report = shacl.execute(graph)
+    puts report
+
 end
--- QSF/main.rb
+++ QSF/main.rb
@@ -1,4 +1,8 @@
 require "./Query_Matching.rb"
 
-test = fake_data_generator("sw:protein", "sw:taxon", "sw:NCBI_taxon")
-print test
+test = fake_data_generator("http://swisslipids.org/rdf#Protein", "http://swisslipids.org/rdf#taxon", "http://swisslipids.org/rdf#NCBI_Taxonomy_Term", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "output_test.ttl")
+
+puts test2
+
