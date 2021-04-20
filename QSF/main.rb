a3796338049d108b02364f85354859ff59ee487c
Acb897 <camara.alber@gmail.com>
2021-04-20 19:43:29 +0200

    Added the check for the results of the validation

--- QSF/main.rb
+++ QSF/main.rb
@@ -4,5 +4,8 @@ test = fake_data_generator("http://swisslipids.org/rdf#Protein", "http://swissli
 
 test2 = shacl_validator("fake_data.ttl", "output_test.ttl")
 
-puts test2
 
+print test2.count
+
+#if test2.all_results.length > 0 #The length of the all_results attribute is 0 if there is no match and >0 when there is a match
+    
