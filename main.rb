e32fa5cdcacff0206de3952610e77cd9ca015906
Acb897 <camara.alber@gmail.com>
2021-04-28 00:46:48 +0200

    Added the fake data gen and validation to the main code

--- main.rb
+++ main.rb
@@ -1,11 +1,18 @@
 require "./SPO_pattern_class.rb"
-
+require "./Query_Matching_class.rb"
 
 engine = Engine.new()
 
 endpoint = "https://rdf.metanetx.org/sparql"
 #endpoint = "http://sparql.uniprot.org/sparql"
-first_test = engine.extract_patterns(endpoint)
-#first_test = engine.extract_patterns("http://sparql.uniprot.org/sparql")
-shacl_test = engine.shacl_generator(first_test, "test.ttl")
+rdf_index = engine.extract_patterns(endpoint)
+shacl_index_gen = engine.shacl_generator(rdf_index, "metanetx.ttl")
 
+fake_data_gen = fake_data_generator("https://rdf.metanetx.org/schema/CPLX", "https://rdf.metanetx.org/schema/subu", "https://rdf.metanetx.org/schema/PEPT", "fake_data1.ttl")
+
+validator = shacl_validator("fake_data1.ttl", "metanetx.ttl")
+
+
+print validator.all_results
+
+#if test2.all_results.length > 0 #The length of the all_results attribute is 0 if there is no match and >0 when there is a match
