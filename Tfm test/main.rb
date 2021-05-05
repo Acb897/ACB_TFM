7ff3b9a279dc03e42e782a53503b27044bf42fcb
Acb897 <camara.alber@gmail.com>
2021-05-05 23:31:35 +0200

    Added demo for the problem with the validation

--- Tfm test/main.rb
+++ Tfm test/main.rb
@@ -2,61 +2,161 @@
 #that the main problem is whatever I did wrong that is causing the "message: "is of class http://www.w3.org/1999/02/22-rdf-syntax-ns#nil" and message: "is not of class http://www.w3.org/1999/02/22-rdf-syntax-ns#nil"
 require "./Query_Matching.rb"
 
-#This is the first example I used, which works correctly. It creates fake RDF data, stores it in a document called fake_data.ttl
-puts "==============================================================="
-puts "Validating the SHACL database against Swisslipids protein data"
-test = fake_data_generator("http://swisslipids.org/rdf#Protein", "http://swisslipids.org/rdf#taxon", "http://swisslipids.org/rdf#NCBI_Taxonomy_Term", "fake_data.ttl")
-
-#Then, it validates the fake data against the shacl database. test2 is a ValidationReport instance from the shacl gem, and by using .all_results it returns all results
-#, both conforming and non-conforming. As you can see in the output, this example doesn't conform to the RDF data. Do you understand why it says "is not of class http://www.w3.org/1999/02/22-rdf-syntax-ns#nil"
-test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
-
-puts "Does this data conform to the SHACL shapes?:"
-puts test2.conform?
-puts puts
-puts test2.all_results
-
-
-
-#Now, when I try to do the same as above but with the subject, predicate and object having Metanext URIs, it conforms, but because it says that "is of class http://www.w3.org/1999/02/22-rdf-syntax-ns#nil"
-puts
-puts "==============================================================="
-puts "Validating the same SHACL database against metanext reaction data"
-test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/right", "https://rdf.metanetx.org/schema/PART", "fake_data.ttl")
-    
-test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
-
-puts "Does this data conform to the SHACL shapes?:"
-puts test2.conform?
-puts puts
-puts test2.all_results
-
-
-
-#If I use a triple that has swisslipids data mixed with metanetx data, the results are different.
-puts 
-puts "==============================================================="
-puts "Validating the same SHACL database against metanetx and swisslipid data"
-test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
-test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
-
-puts "Does this data conform to the SHACL shapes?:"
-puts test2.conform?
-puts puts
-puts test2.all_results
-
-
-
 #If I do the same I did in the first example, but with a database that only contains the Protein shape from swisslipids, I get different results!
 puts
 puts "==============================================================="
-puts "Validating the reduced SHACL database against Swisslipids protein data"
-test = fake_data_generator("http://swisslipids.org/rdf#Protein", "http://swisslipids.org/rdf#taxon", "http://swisslipids.org/rdf#NCBI_Taxonomy_Term", "fake_data.ttl")
+puts "Validating the reduced SHACL database against Metanetx REAC data"
 
-test2 = shacl_validator("fake_data.ttl", "perro2.ttl")
+test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
 
-puts "Does this data conform to the SHACL shapes?:"
-puts test2.conform?
+test2 = shacl_validator("fake_data.ttl", "reduced_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
 puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
 puts test2.all_results
 
+#Now, doing the same as before but changing the subject so it doesn't match. It validates, but there aren't any results.
+puts
+puts "==============================================================="
+puts "Validating the reduced SHACL database against Metanetx REAC data, but with a different subject"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REACa", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "reduced_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now changing the predicate so it doesn't match, and the subject and object DO match.
+puts
+puts "==============================================================="
+puts "Validating the reduced SHACL database against Metanetx REAC data, but with a different predicate"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXrefa", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "reduced_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now changing the object so it doesn't match, and the subject and predicate DO match.
+puts
+puts "==============================================================="
+puts "Validating the reduced SHACL database against Metanetx REAC data, but with a different object"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReferencea", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "reduced_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now changing both the S and P so that they don't match.
+puts
+puts "==============================================================="
+puts "Validating the reduced SHACL database against Metanetx REAC data, but with both Subject and Predicate not matching"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REACa", "https://rdf.metanetx.org/schema/reacXrefa", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "reduced_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+puts
+puts "Basically, if all three parts of the triple match the shape, the shacl::ValidationReport .conform? returns true and the amount of results is >0.
+If some of the parts doesn't match the shape, this no longer holds true"
+
+puts "=============================================================================================="
+puts "                          Testing with the whole shapes database  "
+puts "=============================================================================================="
+puts
+puts "==============================================================="
+puts "Validating the complete SHACL database against Metanetx REAC data"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now, doing the same as before but changing the subject so it doesn't match. It validates, but there aren't any results.
+puts
+puts "==============================================================="
+puts "Validating the complete SHACL database against Metanetx REAC data, but with a different subject"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REACa", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now changing the predicate so it doesn't match, and the subject and object DO match.
+puts
+puts "==============================================================="
+puts "Validating the complete SHACL database against Metanetx REAC data, but with a different predicate"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXrefa", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now changing the object so it doesn't match, and the subject and predicate DO match.
+puts
+puts "==============================================================="
+puts "Validating the complete SHACL database against Metanetx REAC data, but with a different object"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REAC", "https://rdf.metanetx.org/schema/reacXref", "http://swisslipids.org/rdf#DbReferencea", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+#Now changing both the S and P so that they don't match.
+puts
+puts "==============================================================="
+puts "Validating the complete SHACL database against Metanetx REAC data, but with both Subject and Predicate not matching"
+
+test = fake_data_generator("https://rdf.metanetx.org/schema/REACa", "https://rdf.metanetx.org/schema/reacXrefa", "http://swisslipids.org/rdf#DbReference", "fake_data.ttl")
+
+test2 = shacl_validator("fake_data.ttl", "Old_shacl_database.ttl")
+
+puts "Does this data conform to the SHACL shapes?: #{test2.conform?}"
+puts puts
+puts "Showing all (#{test2.all_results.length}) results (conforming and non-conforming)"
+puts
+puts test2.all_results
+
+puts "In this case, the data never conforms to the shapes because of the problem you told me, but only when all parts of the triple match the shape we get 2 results.
+These results are much more 'shaky', and I still don't understand them"
