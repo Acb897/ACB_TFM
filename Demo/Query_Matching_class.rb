b225c962db7cf03ffc9fc3019a0f79cf5bc59325
Acb897 <camara.alber@gmail.com>
2021-05-29 21:11:04 +0200

    Added the query parser function

--- Demo/Query_Matching_class.rb
+++ Demo/Query_Matching_class.rb
@@ -1,5 +1,7 @@
 require 'linkeddata'
 require 'shacl'
+require 'sparql'
+require 'uri'
 
 # Generates mock RDF data to be validated against SHACL shapes.
 #
@@ -8,11 +10,47 @@ require 'shacl'
 # @param object_type [String] the full URI of the rdf:type of the object of the triple.
 # @param output_document [String] the name of the document that will contain the mock data.
 # @return [data] the fake data.
-def fake_data_generator(subject_type, predicate, object_type, output_document)
-    data = "_:ABC a <#{subject_type}> . \n_:ABC <#{predicate}> _:123 . \n_:123 a <#{object_type}> ."
+def fake_data_generator(query, output_document)
+    parsed = SPARQL.parse(query)  # this is a nightmare method, that returns a wide variety of things! LOL!
+    rdf_query=''
+    if parsed.is_a?(RDF::Query)  # we need to get the RDF:Query object out of the list of things returned from the parse
+        rdf_query = parsed
+    else
+        parsed.each {|c| rdf_query = c if c.is_a?(RDF::Query)  }
+    end
+
+    patterns = rdf_query.patterns  # returns the triple patterns in the query
+
+    variables = Hash.new  # we're going to create a random string for every variable in the query
+    patterns.each do |p|  
+        vars = p.unbound_variables  # vars contains e.g. [:s, #<RDF::Query::Variable:0x6a400(?s)>] 
+        vars.each {|var| variables[var[0]] = RDF::URI("http://fakedata.org/" + (0...10).map { ('a'..'z').to_a[rand(26)] }.join)}
+        # now variables[:s] = <http://fakedata.org/adjdsihfrke>
+    end
+
     File.open(output_document, "w") {|file|
-        file.write data}
-    return data
+        # now iterate over the patterns again, and bind them to their new value
+        patterns.each do |triple|  # we're going to create a random string for every variable
+            if triple.subject.variable?
+                var_symbol = triple.subject.to_sym # covert the variable into a symbol, since that is our hash key
+                triple.subject = variables[var_symbol]  # assign the random string for that symbol
+            end
+
+            if triple.predicate.variable?
+                var_symbol = triple.predicate.to_sym # covert the variable into a symbol, since that is our hash key
+                triple.predicate = variables[var_symbol]  # assign the random string for that symbol
+            end
+            
+            # special case for objects, since they can be literals
+            if triple.object.variable?
+                var_symbol = triple.object.to_sym # covert the variable into a symbol, since that is our hash key
+                triple.object = variables[var_symbol]  # assign the random string for that symbol
+                file.write triple.to_rdf
+                file.write "\n"
+            end
+        end
+    }
+
 end
 
 # Uses the SHACL and linkeddata gems to validate the fake data against a database of SHACL shapes.
@@ -21,8 +59,9 @@ end
 # @param shacl_document [String] the name of the document with the SHACL shapes.
 # @return [report] the report of the validation. Check SHACL::ValidationReport from the SHACL gem.
 def shacl_validator(rdf_graph, shacl_document)
-    graph = RDF::Graph.load(rdf_graph)
     shacl_shapes = Hash.new
+    responsive_endpoints = Array.new
+    graph = RDF::Graph.load(rdf_graph)
     File.foreach(shacl_document) {|line|
         case line
             when /^EU/
@@ -38,8 +77,30 @@ def shacl_validator(rdf_graph, shacl_document)
                 next
         end
     }   
-    print shacl_shapes
-    # shacl = SHACL.open(shacl_document)
-    # report = shacl.execute(graph)
-    # return report
+    #This splits the value of the hash, which corresponds to all the shapes from an
+    # endpoint one after another in a string. The split is made on the ] . that
+    # delimits the end of a shape, and with ?<= it is kept in the shapes.
+    shacl_shapes.update(shacl_shapes) {|key, value| value.split(/(?<=\] \.)\n/)}
+    shacl_shapes.each do |key, value|
+        value.each do |shape|
+            File.open("tmp.ttl", "w") {|file|
+            file.write "@prefix sh: <http://www.w3.org/ns/shacl#> .\n\n"
+            file.write shape
+            }
+            shacl = SHACL.open("tmp.ttl")
+            report = shacl.execute(graph)
+            
+            puts "SHAPE:\n\n#{shape}\n\nConforms?: #{report.conform?}\n\nLength: #{report.all_results.length}"
+            puts puts
+            puts report
+            puts puts
+            if report.conform? && report.all_results.length > 0
+                responsive_endpoints << key
+                break
+            else 
+                next
+            end
+        end
+    end
+    return responsive_endpoints
 end
